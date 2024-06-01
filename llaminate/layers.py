f47221292195310ccaa2f6150ebaaed140e53262
apehex <apehex@protonmail.com>
2024-06-01 22:02:40 +0200

    Draft the decoder block, without cache

--- llaminate/layers.py
+++ llaminate/layers.py
@@ -37,41 +37,57 @@ class FeedForwardBlock(tf.keras.layers.Layer):
     def from_config(cls, config) -> tf.keras.layers.Layer:
         return cls(**config)
 
-# TRANSFORMER #################################################################
+# DECODER #####################################################################
 
-class TransformerBlock(tf.keras.layers.Layer):
+EPSILON = 1e-5
+
+@keras.saving.register_keras_serializable(package='blocks')
+class DecoderBlock(tf.keras.layers.Layer):
     def __init__(
         self,
-        layer_id: int,
-        dim: int,
-        n_heads: int,
-        epsilon: float,
-        multiple_of: int,
-        ffn_dim_multiplier: float=1.,
+        num_heads: int,
+        num_kv_heads: int,
+        embed_dim: int,
+        head_dim: int,
+        hidden_dim: int,
         **kwargs
     ) -> None:
-        super(TransformerBlock, self).__init__(name=str(layer_id), **kwargs)
+        # init
+        super(DecoderBlock, self).__init__(**kwargs)
         # config
         self._config = {
-            'layer_id': layer_id,
-            'dim': dim,
-            'n_heads': n_heads,
-            'epsilon': epsilon,
-            'multiple_of': multiple_of,
-            'ffn_dim_multiplier': ffn_dim_multiplier,}
+            'num_heads': num_heads,
+            'num_kv_heads': num_kv_heads,
+            'embed_dim': embed_dim,
+            'head_dim': head_dim,
+            'hidden_dim': hidden_dim,}
         # layers
-        self._attention_norm = tf.keras.layers.LayerNormalization(axis=-1, epsilon=epsilon, rms_scaling=True)
-        self._attention = tf.keras.layers.MultiHeadAttention(num_heads=n_heads, key_dim=dim // n_heads, value_dim=dim // n_heads, use_bias=False, kernel_initializer='glorot_uniform')
-        self._ffn_norm = tf.keras.layers.LayerNormalization(axis=-1, epsilon=epsilon, rms_scaling=True)
-        self._ffn = FeedForwardBlock(dim=dim, hidden_dim=4 * dim, multiple_of=multiple_of, ffn_dim_multiplier=ffn_dim_multiplier)
+        self._attention_norm = tf.keras.layers.LayerNormalization(axis=-1, epsilon=EPSILON, rms_scaling=True, gamma_initializer='ones') # RMS
+        self._position = mlable.layers.embedding.RotaryPositionalEmbedding(head_dim=head_dim)
+        self._attention = tf.keras.layers.MultiHeadAttention(num_heads=num_heads, key_dim=head_dim, value_dim=head_dim, use_bias=False, kernel_initializer='glorot_uniform')
+        self._ffn_norm = tf.keras.layers.LayerNormalization(axis=-1, epsilon=EPSILON, rms_scaling=True, gamma_initializer='ones') # RMS
+        self._ffn = FeedForwardBlock(input_dim=embed_dim, hidden_dim=hidden_dim)
 
-    def call(self, inputs: tf.Tensor) -> tf.Tensor:
-        __t = self._attention_norm(inputs)
-        __t = inputs + self._attention(query=__t, value=__t, key=__t, use_causal_mask=True)
-        return __t + self._ffn(self._ffn_norm(__t))
+    def call(self, inputs: tf.Tensor, positions: tf.Tensor, cache: tf.Tensor, mask: tf.Tensor) -> tf.Tensor:
+        # residual
+        __x = inputs
+        # normalize
+        __y = self._attention_norm(__x)
+        # position embedding
+        __y = self._position(inputs=__y, positions=positions)
+        # attention
+        __y = self._attention(key=__y, query=__y, value=__y, use_causal_mask=True)
+        # residual
+        __x = __y + __x
+        # normalize
+        __y = self._ffn_norm(__x)
+        # augment
+        __y = self._ffn(__y)
+        # residual
+        return __y + __x
 
     def get_config(self) -> dict:
-        __parent_config = super(FeedForwardBlock, self).get_config()
+        __parent_config = super(DecoderBlock, self).get_config()
         return {**__parent_config, **self._config}
 
     @classmethod
