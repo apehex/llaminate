09d55f776a680c4f1f420195ad45a26f2483368a
apehex <apehex@protonmail.com>
2024-06-26 14:47:41 +0200

    Fix missing deps

--- llaminate/pipeline.py
+++ llaminate/pipeline.py
@@ -1,17 +1,20 @@
 import tensorflow as tf
 
+import mlable.data
+import tokun.pipeline
+
 # DISTRIBUTE ##################################################################
 
 _map = lambda __f: (lambda *__t: tuple(map(__f, __t)))
 
 # PREPROCESS ##################################################################
 
-def preprocess(dataset: tf.data.Dataset, token_dim: int, embed_dim: int, sample_dim: int, features: list=[]) -> tf.data.Dataset:
+def preprocess(dataset: tf.data.Dataset, token_dim: int, embed_dim: int, sample_dim: int, features: list=[], separator: str='\x1d') -> tf.data.Dataset:
     __pipeline = [
         # join the features
-        ((lambda __x: tf.strings.join(inputs=[__x[__f] for __f in features], separator='\x1d') if features else __x), True),
+        ((lambda __x: tf.strings.join(inputs=[__x[__f] for __f in features], separator=separator) if features else __x), True),
         # (input, target) where target is the next character for each input
-        ((lambda __x: (tokun.pipeline.offset(data=__x, ticks=16), __x)), True),
+        ((lambda __x: (tokun.pipeline.offset(data=__x, ticks=token_dim // 4), __x)), True),
         # encode => (4 * S,) int
         (_map(functools.partial(tokun.pipeline.encode, token_size=token_dim, sample_size=sample_dim)), True),
         # reshape => (4 * S,) int
