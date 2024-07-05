Distinguish attention mask from sample mask + fix typos + tidy up

llaminate/layers.py
@@ -44,7 +44,7 @@ class CacheDecoderBlock(tf.keras.layers.Layer):
         self,
         inputs: tf.Tensor,
         cache: tf.Tensor=None,
-        mask: tf.Tensor=None,
+        attention_mask: tf.Tensor=None,
         position: int=0,
         training: bool=False,
     ) -> tf.Tensor:
@@ -55,7 +55,7 @@ class CacheDecoderBlock(tf.keras.layers.Layer):
         # position embedding
         __yp = self._position(inputs=__y, offset=position)
         # attention
-        __y, __cache = self._attention(key=__yp, query=__yp, value=__y, cache=cache, step=position, training=training, attention_mask=mask, use_causal_mask=True, return_attention_scores=False)
+        __y, __cache = self._attention(key=__yp, query=__yp, value=__y, cache=cache, step=position, training=training, attention_mask=attention_mask, use_causal_mask=True, return_attention_scores=False)
         # residual
         __x = __y + __x
         # normalize
@@ -107,7 +107,7 @@ class DecoderBlock(tf.keras.layers.Layer):
     def call(
         self,
         inputs: tf.Tensor,
-        mask: tf.Tensor=None,
+        attention_mask: tf.Tensor=None,
         training: bool=False,
     ) -> tf.Tensor:
         # residual
@@ -117,7 +117,7 @@ class DecoderBlock(tf.keras.layers.Layer):
         # position embedding
         __yp = self._position(inputs=__y, offset=0)
         # attention
-        __y = self._attention(key=__yp, query=__yp, value=__y, training=training, attention_mask=mask, use_causal_mask=True, return_attention_scores=False)
+        __y = self._attention(key=__yp, query=__yp, value=__y, training=training, attention_mask=attention_mask, use_causal_mask=True, return_attention_scores=False)
         # residual
         __x = __y + __x
         # normalize

llaminate/model.py
@@ -50,12 +50,12 @@ class CacheTransformer(tf.keras.models.Model):
         self._norm = tf.keras.layers.LayerNormalization(axis=-1, epsilon=epsilon, beta_initializer='zeros', gamma_initializer='ones') # rms_scaling=True, 
         self._decoder = None
 
-    def call(self, inputs: tf.Tensor, **kwargs) -> tf.Tensor:
+    def call(self, inputs: tf.Tensor, attention_mask: tf.Tensor=None, **kwargs) -> tf.Tensor:
         # byte embedding
         __y = self._encoder(inputs) if self._encoder is not None else inputs
         # blocks
         for __block in self._blocks:
-            __y, _ = __block(inputs=__y, position=0, training=True, cache=None, mask=None)
+            __y, _ = __block(inputs=__y, attention_mask=attention_mask, position=0, training=True, cache=None)
         # normalize
         __y = self._norm(__y)
         # decompress
@@ -66,7 +66,7 @@ class CacheTransformer(tf.keras.models.Model):
     def infer(
         self,
         inputs: tf.Tensor,
-        mask: tf.Tensor=None,
+        attention_mask: tf.Tensor=None,
         cache: list=None,
         position: int=0,
         **kwargs,
@@ -77,7 +77,7 @@ class CacheTransformer(tf.keras.models.Model):
         __y = self._encoder(inputs) if self._encoder is not None else inputs
         # blocks
         for __i, __block in enumerate(self._blocks):
-            __y, __cache[__i] = __block(inputs=__y, cache=__cache[__i], mask=mask, position=position, training=False)
+            __y, __cache[__i] = __block(inputs=__y, cache=__cache[__i], attention_mask=attention_mask, position=position, training=False)
         # normalize
         __y = self._norm(__y)
         # decompress
@@ -140,12 +140,12 @@ class Transformer(tf.keras.models.Model):
         self._norm = tf.keras.layers.LayerNormalization(axis=-1, epsilon=epsilon, beta_initializer='zeros', gamma_initializer='ones') # rms_scaling=True, 
         self._decoder = None
 
-    def call(self, inputs: tf.Tensor, **kwargs) -> tf.Tensor:
+    def call(self, inputs: tf.Tensor, attention_mask: tf.Tensor=None, **kwargs) -> tf.Tensor:
         # byte embedding
         __y = self._encoder(inputs) if self._encoder is not None else inputs
         # blocks
         for __block in self._blocks:
-            __y = __block(inputs=__y, **kwargs)
+            __y = __block(inputs=__y, attention_mask=attention_mask, **kwargs)
         # normalize
         __y = self._norm(__y)
         # decompress

tests/test_model.py
@@ -1,6 +1,5 @@
 import math
 
-import numpy as np
 import tensorflow as tf
 
 import tokun.model
@@ -13,7 +12,6 @@ import llaminate.utils
 class CacheTransformerTest(tf.test.TestCase):
 
     def test_shapes(self):
-        """Test with a mask tensor."""
         __meta = {
             'cache': {
                 'num_layers': 2,
@@ -26,8 +24,7 @@ class CacheTransformerTest(tf.test.TestCase):
                 'feature_axis': -1,
                 'token_dim': [4, 4],
                 'encoding_dim': 256,
-                'embedding_dim': 256,
-                'latent_dim': 256,},
+                'embedding_dim': 256,},
             'llaminate': {
                 'num_layers': 2,
                 'num_heads': 4,
@@ -51,26 +48,24 @@ class CacheTransformerTest(tf.test.TestCase):
         # checks
         self.assertEqual(tf.argmax(__y, axis=-1).shape, __x.shape)
         # infer
-        __y, __c = __m.infer(inputs=__x, cache=__c, mask=None, position=4, training=False)
+        __y, __c = __m.infer(inputs=__x, cache=__c, attention_mask=None, position=4, training=False)
         # checks
         self.assertEqual(tf.argmax(__y, axis=-1).shape, __x.shape)
-        self.assertEqual(len(__c), __meta['num_layers'])
-        self.assertEqual(__c[0].shape, (2, __meta['batch_dim'], __meta['cache_dim'], __meta['num_heads'], __meta['head_dim']))
+        self.assertEqual(len(__c), __meta['llaminate']['num_layers'])
+        self.assertEqual(__c[0].shape, (2, __meta['cache']['batch_dim'], __meta['cache']['cache_dim'], __meta['cache']['num_heads'], __meta['cache']['head_dim']))
 
 # WITH CACHE ##################################################################
 
-class CacheTransformerTest(tf.test.TestCase):
+class TransformerTest(tf.test.TestCase):
 
     def test_shapes(self):
-        """Test with a mask tensor."""
         __meta = {
             'tokun': {
                 'sequence_axis': 1,
                 'feature_axis': -1,
                 'token_dim': [4, 4],
                 'encoding_dim': 256,
-                'embedding_dim': 256,
-                'latent_dim': 256,},
+                'embedding_dim': 256,},
             'llaminate': {
                 'num_layers': 2,
                 'num_heads': 4,
@@ -88,6 +83,6 @@ class CacheTransformerTest(tf.test.TestCase):
         # inputs
         __x = tf.ones((1, math.prod(__meta['tokun']['token_dim']) * __meta['llaminate']['cache_dim']))
         # call
-        __y = __m.call(inputs=__x, training=False)
+        __y = __m.call(inputs=__x, attention_mask=None, training=False)
         # checks
         self.assertEqual(tf.argmax(__y, axis=-1).shape, __x.shape)