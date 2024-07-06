8fdf865b1dbe295c5d05e413f9ca3d67d9f82b25
apehex <apehex@protonmail.com>
2024-07-06 22:04:11 +0200

    Cast the weights from bool to float32

--- llaminate/pipeline.py
+++ llaminate/pipeline.py
@@ -7,7 +7,7 @@ import tokun.pipeline
 
 # PREPROCESS ##################################################################
 
-def preprocess(inputs: tf.Tensor, token_dim: int, embed_dim: int, batch_dim: int, sample_dim: int, features: list, separator: str='\x1d', mask: bool=True) -> tf.data.Dataset:
+def preprocess(inputs: tf.Tensor, token_dim: int, embed_dim: int, batch_dim: int, sample_dim: int, features: list, separator: str='\x1d', weight: bool=True) -> tf.data.Dataset:
     # specialized operations
     __encode = functools.partial(tokun.pipeline.encode, token_size=token_dim, sample_size=sample_dim)
     __reshape = functools.partial(tf.reshape, shape=(batch_dim, 4 * sample_dim))
@@ -22,7 +22,8 @@ def preprocess(inputs: tf.Tensor, token_dim: int, embed_dim: int, batch_dim: int
     # one-hot encoding for the targets => (B, 4 * S, E) int (bool)
     __inputs, __targets = __inputs, tf.one_hot(__targets, depth=embed_dim, axis=-1)
     # sequence mask to ignore padding during training
-    __mask = tf.not_equal(__inputs, 0) # byte level mask
-    __mask = mlable.masking.reduce_any(mask=__mask, group=4, axis=-1, keepdims=True) # character level mask, but expressed byte by byte
+    __weights = tf.not_equal(__inputs, 0) # byte level mask
+    __weights = mlable.masking.reduce_any(mask=__weights, group=4, axis=-1, keepdims=True) # character level mask, but expressed byte by byte
+    __weights = tf.cast(__weights, dtype=__targets.dtype)
     # chain the operations
-    return (__inputs, __targets, __mask) if mask else (__inputs, __targets)
+    return (__inputs, __targets, __weights) if weight else (__inputs, __targets)
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -10,19 +10,19 @@ import llaminate.pipeline
 class PreprocessTest(tf.test.TestCase):
     def setUp(self):
         super(PreprocessTest, self).setUp()
-        self._config = {'batch_dim': 8, 'sample_dim': 128, 'token_dim': 16, 'embed_dim': 256, 'mask': True, 'features': ['question']}
+        self._config = {'batch_dim': 8, 'sample_dim': 128, 'token_dim': 16, 'embed_dim': 256, 'weight': True, 'features': ['question']}
         self._preprocess = functools.partial(llaminate.pipeline.preprocess, **self._config)
         self._dataset_before = tfds.load('mlqa/en', split='test', as_supervised=False, shuffle_files=True, data_dir='~/.cache/tensorflow/', batch_size=None)
         self._dataset_after = self._dataset_before.batch(self._config['batch_dim']).map(self._preprocess)
 
     def test_specs(self):
-        __inputs_spec, __targets_spec, __masks_spec = self._dataset_after.element_spec
+        __inputs_spec, __targets_spec, __weights_spec = self._dataset_after.element_spec
         self.assertEqual(__inputs_spec.shape, (self._config['batch_dim'], 4 * self._config['sample_dim']))
         self.assertEqual(__targets_spec.shape, (self._config['batch_dim'], 4 * self._config['sample_dim'], self._config['embed_dim']))
-        self.assertEqual(__masks_spec.shape, (self._config['batch_dim'], 4 * self._config['sample_dim']))
+        self.assertEqual(__weights_spec.shape, (self._config['batch_dim'], 4 * self._config['sample_dim']))
         self.assertEqual(__inputs_spec.dtype, tf.dtypes.uint8)
         self.assertEqual(__targets_spec.dtype, tf.dtypes.float32)
-        self.assertEqual(__masks_spec.dtype, tf.dtypes.bool)
+        self.assertEqual(__weights_spec.dtype, tf.dtypes.float32)
 
     def test_values(self):
         __batch = iter(self._dataset_after)
@@ -33,7 +33,7 @@ class PreprocessTest(tf.test.TestCase):
             self.assertAllEqual(__x[:, :self._config['token_dim']], tf.zeros(shape=(self._config['batch_dim'], self._config['token_dim']), dtype=tf.dtypes.int32))
             self.assertAllEqual(__x[:, self._config['token_dim']:], __y[:, :-self._config['token_dim']]) # x and y are offset by token_dim
 
-    def test_masks(self):
+    def test_weights(self):
         __batch = iter(self._dataset_after)
         for _ in range(16):
             __x, __y, __m = next(__batch)
