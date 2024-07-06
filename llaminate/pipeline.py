7c6fcc638555a680ee00ed9fa3b7c874d73a6360
apehex <apehex@protonmail.com>
2024-07-06 12:01:33 +0200

    Mask the padding in the inputs => ignore in the loss and metric fn

--- llaminate/pipeline.py
+++ llaminate/pipeline.py
@@ -2,12 +2,12 @@ import functools
 
 import tensorflow as tf
 
-import mlable.utils
+import mlable.masking
 import tokun.pipeline
 
 # PREPROCESS ##################################################################
 
-def preprocess(inputs: tf.Tensor, token_dim: int, embed_dim: int, batch_dim: int, sample_dim: int, features: list, separator: str='\x1d') -> tf.data.Dataset:
+def preprocess(inputs: tf.Tensor, token_dim: int, embed_dim: int, batch_dim: int, sample_dim: int, features: list, separator: str='\x1d', mask: bool=True) -> tf.data.Dataset:
     # specialized operations
     __encode = functools.partial(tokun.pipeline.encode, token_size=token_dim, sample_size=sample_dim)
     __reshape = functools.partial(tf.reshape, shape=(batch_dim, 4 * sample_dim))
@@ -15,11 +15,14 @@ def preprocess(inputs: tf.Tensor, token_dim: int, embed_dim: int, batch_dim: int
     __inputs = tf.strings.join(inputs=[inputs[__f] for __f in features], separator=separator)
     # (input, target) where target is the next token for each input
     __inputs, __targets = (tokun.pipeline.offset(data=__inputs, ticks=token_dim // 4), __inputs)
-    # encode => (4 * S,) int
+    # encode => (B, 4 * S,) int
     __inputs, __targets = (__encode(__inputs), __encode(__targets))
-    # reshape => (4 * S,) int
+    # reshape => (B, 4 * S,) int
     __inputs, __targets = (__reshape(__inputs), __reshape(__targets))
-    # one-hot encoding for the targets => (4 * S, E) int (bool)
+    # one-hot encoding for the targets => (B, 4 * S, E) int (bool)
     __inputs, __targets = __inputs, tf.one_hot(__targets, depth=embed_dim, axis=-1)
+    # sequence mask to ignore padding during training
+    __mask = tf.not_equal(__inputs, 0) # byte level mask
+    __mask = mlable.masking.reduce_any(mask=__mask, group=4, axis=-1, keepdims=True) # character level mask, but expressed byte by byte
     # chain the operations
-    return (__inputs, __targets)
+    return (__inputs, __targets, __mask) if mask else (__inputs, __targets)
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -10,22 +10,36 @@ import llaminate.pipeline
 class PreprocessTest(tf.test.TestCase):
     def setUp(self):
         super(PreprocessTest, self).setUp()
-        self._config = {'batch_dim': 8, 'sample_dim': 128, 'token_dim': 16, 'embed_dim': 256, 'features': ['context', 'question']}
+        self._config = {'batch_dim': 8, 'sample_dim': 128, 'token_dim': 16, 'embed_dim': 256, 'mask': True, 'features': ['question']}
         self._preprocess = functools.partial(llaminate.pipeline.preprocess, **self._config)
         self._dataset_before = tfds.load('mlqa/en', split='test', as_supervised=False, shuffle_files=True, data_dir='~/.cache/tensorflow/', batch_size=None)
         self._dataset_after = self._dataset_before.batch(self._config['batch_dim']).map(self._preprocess)
 
     def test_specs(self):
-        __inputs_spec, __targets_spec = self._dataset_after.element_spec
+        __inputs_spec, __targets_spec, __masks_spec = self._dataset_after.element_spec
         self.assertEqual(__inputs_spec.shape, (self._config['batch_dim'], 4 * self._config['sample_dim']))
         self.assertEqual(__targets_spec.shape, (self._config['batch_dim'], 4 * self._config['sample_dim'], self._config['embed_dim']))
+        self.assertEqual(__masks_spec.shape, (self._config['batch_dim'], 4 * self._config['sample_dim']))
         self.assertEqual(__inputs_spec.dtype, tf.dtypes.uint8)
         self.assertEqual(__targets_spec.dtype, tf.dtypes.float32)
+        self.assertEqual(__masks_spec.dtype, tf.dtypes.bool)
 
     def test_values(self):
         __batch = iter(self._dataset_after)
-        __x, __y = next(__batch)
-        __x = tf.cast(__x, dtype=tf.dtypes.int32)
-        __y = tf.argmax(__y, axis=-1, output_type=tf.dtypes.int32)
-        self.assertAllEqual(__x[:, :self._config['token_dim']], tf.zeros(shape=(self._config['batch_dim'], self._config['token_dim']), dtype=tf.dtypes.int32))
-        self.assertAllEqual(__x[:, self._config['token_dim']:], __y[:, :-self._config['token_dim']]) # x and y are offset by token_dim
+        for _ in range(16):
+            __x, __y, __m = next(__batch)
+            __x = tf.cast(__x, dtype=tf.dtypes.int32)
+            __y = tf.argmax(__y, axis=-1, output_type=tf.dtypes.int32)
+            self.assertAllEqual(__x[:, :self._config['token_dim']], tf.zeros(shape=(self._config['batch_dim'], self._config['token_dim']), dtype=tf.dtypes.int32))
+            self.assertAllEqual(__x[:, self._config['token_dim']:], __y[:, :-self._config['token_dim']]) # x and y are offset by token_dim
+
+    def test_masks(self):
+        __batch = iter(self._dataset_after)
+        for _ in range(16):
+            __x, __y, __m = next(__batch)
+            __x = tf.cast(__x, dtype=tf.dtypes.int32)
+            __y = tf.argmax(__y, axis=-1, output_type=tf.dtypes.int32)
+            __m = tf.cast(__m, dtype=tf.dtypes.int32)
+            assert 0 < tf.reduce_sum(__m).numpy()
+            assert tf.reduce_sum(__m).numpy() < tf.size(__m).numpy()
+            self.assertAllEqual(__x * __m, __x)
